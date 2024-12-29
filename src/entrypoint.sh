5db72d06fd77f89b4ce2c79597901a037d44de20
rtsarakaki <rtsarakaki@gmail.com>
2024-12-29 15:57:58 -0300

    feat(entrypoint.sh): Simplifies the logic using aws cloudformation deploy

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,16 +6,9 @@ TEMPLATE_FILE=$2
 AWS_REGION=$3
 DRY_RUN=$4
 
-if aws cloudformation describe-stacks --stack-name $STACK_NAME --region $AWS_REGION 2>/dev/null; then
-  echo "Stack $STACK_NAME encontrada. Atualizando..."
-  aws cloudformation update-stack \
+aws cloudformation deploy \
     --stack-name $STACK_NAME \
-    --template-body file://$TEMPLATE_FILE \
+    --template-file $TEMPLATE_FILE \
     --region $AWS_REGION
-else
-  echo "Stack $STACK_NAME não encontrada. Criando..."
-  aws cloudformation create-stack \
-    --stack-name $STACK_NAME \
-    --template-body file://$TEMPLATE_FILE \
-    --region $AWS_REGION
-fi
+    --parameter-overrides UserPassword=${{ secrets.USER_PASSWORD }} \
+    --capabilities CAPABILITY_NAMED_IAM
