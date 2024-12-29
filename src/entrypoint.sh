c609439096878ea099ad3d57eaea5ed25320e80e
rtsarakaki <rtsarakaki@gmail.com>
2024-12-29 16:04:19 -0300

    feat(entrypoint.sh): print the variables to simplify debug

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -6,6 +6,11 @@ TEMPLATE_FILE=$2
 AWS_REGION=$3
 DRY_RUN=$4
 
+echo STACK_NAME
+echo TEMPLATE_FILE
+echo AWS_REGION
+echo DRY_RUN
+
 aws cloudformation deploy \
     --stack-name $STACK_NAME \
     --template-file $TEMPLATE_FILE \
