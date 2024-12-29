928141cb98d773e031554b0efb9552e63255dfb8
rtsarakaki <rtsarakaki@gmail.com>
2024-12-29 16:54:18 -0300

    fix(entrypoint.sh): fix parameter passing sintaxe

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,13 +7,15 @@ USER_PASSWORD=$3
 AWS_REGION=$4
 DRY_RUN=$5
 
-echo $STACK_NAME
-echo $TEMPLATE_FILE
-echo $AWS_REGION
-echo $DRY_RUN
+echo "Stack Name: $STACK_NAME"
+echo "Template File: $TEMPLATE_FILE"
+echo "AWS Region: $AWS_REGION"
+echo "Dry Run: $DRY_RUN"
 
 aws cloudformation deploy \
-    --stack-name $STACK_NAME \
-    --template-file $TEMPLATE_FILE \
-    --parameter-overrides UserPassword=$USER_PASSWORD \
-    --capabilities CAPABILITY_NAMED_IAM
+    --stack-name "$STACK_NAME" \
+    --template-file "$TEMPLATE_FILE" \
+    --parameter-overrides UserPassword="$USER_PASSWORD" \
+    --capabilities CAPABILITY_NAMED_IAM \
+    --region "$AWS_REGION" \
+    --no-execute-changeset $DRY_RUN
